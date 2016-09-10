837532f2c500bccb3813c22ab48c7fcfe8868e19
fenghou <johnhu456@163.com>
2016-09-10 17:50:52 +0800

    Add a method to resize image

--- FHKit/FHKit/UIImage+FHExtension.h
+++ FHKit/FHKit/UIImage+FHExtension.h
@@ -10,7 +10,22 @@
 
 @interface UIImage (FHExtension)
 
-/**裁剪成圆*/
+/**
+ *  Cut an image to a round image
+ *
+ *  @param radius The round's radius
+ *
+ *  @return The round image
+ */
 - (UIImage *)cutToRoundImageWithRadius:(CGFloat)radius;
+
+/**
+ *  Resize the image to a specific size
+ *
+ *  @param size The specific size
+ *
+ *  @return The resized image
+ */
+- (UIImage *)scaleToSize:(CGSize)size;
 
 @end
--- FHKit/FHKit/UIImage+FHExtension.m
+++ FHKit/FHKit/UIImage+FHExtension.m
@@ -29,4 +29,24 @@
     return image;
 }
 
+-(UIImage *)scaleToSize:(CGSize)size
+{
+    // 创建一个bitmap的context，并把它设置成为当前正在使用的context(非高清屏方法)
+    //    UIGraphicsBeginImageContext(size);
+    // 适配高清屏方法
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    
+    // 绘制改变大小的图片
+    [self drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    
+    // 从当前context中创建一个改变大小后的图片
+    UIImage *scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    // 使当前的context出堆栈
+    UIGraphicsEndImageContext();
+    
+    // 返回新的改变大小后的图片
+    return scaledImage;
+}
+
 @end
